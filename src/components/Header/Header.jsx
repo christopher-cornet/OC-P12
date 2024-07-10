af98a3ace50e7c080a2b96f5ee31acb925eaacde
Christopher <tophercrnt@gmail.com>
2024-07-10 11:56:00 +0200

    Add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+// import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <a href="https://christopher-cornet.github.io/OC-P12/">
+                    <p>christopher-cornet.github.io</p>
+                </a>
+            </div>
+            <div className="social-links">
+                <a href="https://github.com/christopher-cornet" target="_blank">
+                    <FontAwesomeIcon icon={faGithub} style={{color: "#c5c8d0"}} />
+                </a>
+                <a href="https://www.linkedin.com/in/christopher-cornet/" target="_blank">
+                    <FontAwesomeIcon icon={faLinkedin} style={{color: "#c5c8d0"}} />
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://christopher-cornet.github.io/OC-P12/",
   plugins: [react()],
 })
