6462e417461c9ccf7fc9b8c464d316c002a73fd1
Christopher <tophercrnt@gmail.com>
2024-07-16 14:53:42 +0200

    Alts

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
           <p>I love creating and bringing things I have in mind to life with code.</p>
         </section>
         <section>
-          <img src={profilePicture} alt="" />
+          <img src={profilePicture} alt="Photo de profil de Christopher Cornet" />
         </section>
       </div>
       <main>
@@ -35,12 +35,12 @@ function App() {
         <GlowCapture>
           <Glow color='hsl(48, 96%, 53%)'>
             <div className="projects">
-              <Card projectPath={projet11} title="Argent Bank" repository="https://github.com/christopher-cornet/OC-P11" website="" />
-              <Card projectPath={projet10} title="724 Events" repository="https://github.com/christopher-cornet/OC-P10" website="" />
-              <Card projectPath={projet8} title="Kasa" repository="https://github.com/christopher-cornet/oc-kasa" website="" />
-              <Card projectPath={projet6} title="Portfolio d'architecte" repository="https://github.com/christopher-cornet/oc-portfolio-architecte" website="" />
-              <Card projectPath={projet5} title="Print-it" repository="https://github.com/christopher-cornet/print-it" website="https://christopher-cornet.github.io/print-it/" />
-              <Card projectPath={projet4} title="Oh My Food" repository="https://github.com/christopher-cornet/ohmyfood" website="https://christopher-cornet.github.io/ohmyfood/" />
+              <Card projectPath={projet11} title="Argent Bank" repository="https://github.com/christopher-cornet/OC-P11" website="" alt="Projet 11 Argent Bank" />
+              <Card projectPath={projet10} title="724 Events" repository="https://github.com/christopher-cornet/OC-P10" website="" alt="Projet 10 724 Events" />
+              <Card projectPath={projet8} title="Kasa" repository="https://github.com/christopher-cornet/oc-kasa" website="" alt="Projet 8 Kasa" />
+              <Card projectPath={projet6} title="Portfolio d'architecte" repository="https://github.com/christopher-cornet/oc-portfolio-architecte" website="" alt="Projet Portfolio d'architecte" />
+              <Card projectPath={projet5} title="Print-it" repository="https://github.com/christopher-cornet/print-it" website="https://christopher-cornet.github.io/print-it/" alt="Projet Print It" />
+              <Card projectPath={projet4} title="Oh My Food" repository="https://github.com/christopher-cornet/ohmyfood" website="https://christopher-cornet.github.io/ohmyfood/" alt="Projet Oh My Food" />
             </div>
           </Glow>
         </GlowCapture>
@@ -74,9 +74,9 @@ function App() {
           <GlowCapture>
             <Glow color='hsl(48, 96%, 53%)'>
             <section className="container_contact-cards">
-              <ContactCard logo={linkedin} alt="" name="Christopher Cornet" description="+500 Connections" contactLink="https://www.linkedin.com/in/christopher-cornet/" />
-              <ContactCard logo={github} alt="" name="@christopher-cornet" description="3 Followers" contactLink="https://github.com/christopher-cornet" />
-              <ContactCard logo={gmail} alt="" name="tophercrnt@gmail.com" description="Email me for any inquiries." contactLink="mailto:tophercrnt@gmail.com" />
+              <ContactCard logo={linkedin} alt="Linkedin" name="Christopher Cornet" description="+500 Connections" contactLink="https://www.linkedin.com/in/christopher-cornet/" />
+              <ContactCard logo={github} alt="Github" name="@christopher-cornet" description="3 Followers" contactLink="https://github.com/christopher-cornet" />
+              <ContactCard logo={gmail} alt="Gmail" name="tophercrnt@gmail.com" description="Email me for any inquiries." contactLink="mailto:tophercrnt@gmail.com" />
             </section>
             </Glow>
           </GlowCapture>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,10 +5,10 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 // Params : title, image, repository, website
-function Card({ projectPath, title, repository, website }) {
+function Card({ projectPath, title, repository, website, alt }) {
     return (
         <section className="card">
-			<img className="projectImg" src={projectPath} alt="" />
+			<img className="projectImg" src={projectPath} alt={alt} />
 			<p className="card_title">{title}</p>
 			<div className="card_icons">
 				<a href={repository} target="_blank">
@@ -20,10 +20,6 @@ function Card({ projectPath, title, repository, website }) {
 					</a>
 				) : null}
 			</div>
-			{/* Titre du projet */}
-			{/* Image du projet */}
-			{/* Icone avec un lien vers le repo du projet */}
-			{/* Icone avec un lien vers le site en ligne si déployé */}
         </section>
     )
 }
@@ -33,6 +29,7 @@ Card.propTypes = {
     title: PropTypes.string.isRequired,
     repository: PropTypes.string.isRequired,
     website: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 };
 
 export default Card;
